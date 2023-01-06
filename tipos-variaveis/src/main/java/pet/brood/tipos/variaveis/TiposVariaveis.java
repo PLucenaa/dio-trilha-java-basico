46facde66e01a0cefea38af839bf5731af7513c2
e0124 <e0124@IPERDELL1098.iniper01.lan>
2023-01-06 12:57:49 -0400

    criando projetos para exemplos de tipos de variaveis

---
+++ tipos-variaveis/src/main/java/pet/brood/tipos/variaveis/TiposVariaveis.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package pet.brood.tipos.variaveis;
+
+/**
+ *
+ * @author e0124
+ */
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
